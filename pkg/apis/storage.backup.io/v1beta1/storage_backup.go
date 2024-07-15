7df425c148661ae43fea59b0ffe71f9ce66e216d
wangweibo <weibo.wang4@gientech.com>
2024-07-15 15:50:19 +0800

    fix storage backup api

--- pkg/apis/storage.backup.io/v1beta1/storage_backup.go
+++ pkg/apis/storage.backup.io/v1beta1/storage_backup.go
@@ -10,11 +10,7 @@ import (
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// +kubebuilder:resource:shortName=vmbackup;vmbackups,scope=Namespaced
-// +kubebuilder:printcolumn:name="SOURCE_KIND",type=string,JSONPath=`.spec.source.kind`
-// +kubebuilder:printcolumn:name="SOURCE_NAME",type=string,JSONPath=`.spec.source.name`
 // +kubebuilder:printcolumn:name="TYPE",type=string,JSONPath=`.spec.type`
-// +kubebuilder:printcolumn:name="READY_TO_USE",type=boolean,JSONPath=`.status.readyToUse`
 // +kubebuilder:printcolumn:name="AGE",type=date,JSONPath=`.metadata.creationTimestamp`
 // +kubebuilder:printcolumn:name="ERROR",type=date,JSONPath=`.status.error.message`
 
@@ -29,13 +25,8 @@ type StorageBackup struct {
 }
 
 type StorageBackupSpec struct {
-	//Source corev1.TypedLocalObjectReference `json:"source"`
-	//
-	//// +kubebuilder:default:="backup"
-	//// +kubebuilder:validation:Enum=backup;snapshot
-	//// +kubebuilder:validation:Optional
-	//Type BackupType `json:"type,omitempty" default:"backup"`
-	VolumeBackups []*VolumeBackup `json:"volumeBackups"`
+	// +optional
+	VolumeBackups []VolumeBackup `json:"volumeBackups"`
 }
 
 type VolumeBackup struct {
